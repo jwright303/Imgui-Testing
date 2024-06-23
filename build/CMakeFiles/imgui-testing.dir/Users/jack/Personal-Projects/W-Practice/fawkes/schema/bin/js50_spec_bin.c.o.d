c8006216839ef9eba83c1be4a746f0929ef397ce
Jack Wright <jackwright@Jacks-MacBook-Air.local>
2024-06-22 20:20:49 -0700

    Changing to new setup

--- build/CMakeFiles/imgui-testing.dir/Users/jack/Personal-Projects/W-Practice/fawkes/schema/bin/js50_spec_bin.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/imgui-testing.dir/Users/jack/Personal-Projects/W-Practice/fawkes/schema/bin/js50_spec_bin.c.o: \
-  /Users/jack/Personal-Projects/W-Practice/fawkes/schema/bin/js50_spec_bin.c
